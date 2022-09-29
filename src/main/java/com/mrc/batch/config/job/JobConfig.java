5385a8681b1bc8ba8cc09c01a65ca223e23a0fe3
Mauricio Carvalho <mauricio.cac@hotmail.com>
2022-09-29 11:36:31 -0300

    Separando as responsabilidades dos step e job

---
+++ src/main/java/com/mrc/batch/config/job/JobConfig.java
@@ -0,0 +1,23 @@
+package com.mrc.batch.config.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class JobConfig {
+	@Autowired
+	private JobBuilderFactory jobFactory;
+
+	@Bean
+	public Job job(Step step) {
+		System.out.println("JOB CONFIGURADO");
+		return jobFactory.get("Job Configurado").start(step).build();
+	}
+
+}
--- src/main/java/com/mrc/batch/config/step/StepConfig.java
+++ src/main/java/com/mrc/batch/config/step/StepConfig.java
@@ -1,10 +1,7 @@
-package com.mrc.batch.config;
+package com.mrc.batch.config.step;
 
-import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -13,23 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@EnableBatchProcessing
 @Configuration
-public class BatchConfig {
+public class StepConfig {
 	@Autowired
-	private JobBuilderFactory jobFactory;
-	@Autowired
-	private StepBuilderFactory stepFactory;
-
-	@Bean
-	public Job job() {
-		System.out.println("JOB CONFIGURADO");
-		return jobFactory.get("Job Configurado").start(step()).build();
-	}
+	private StepBuilderFactory factory;
 
 	@Bean
 	public Step step() {
-		return stepFactory.get("Step Configurado").tasklet(new Tasklet() {
+		return factory.get("step Configurado").tasklet(new Tasklet() {
+
 			@Override
 			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 				System.out.println("STEP CONFIGURADO");
@@ -37,5 +26,4 @@ public class BatchConfig {
 			}
 		}).build();
 	}
-
 }
