1d49150a3e2647a0cbb506ec085d9f6d2706e5c8
Mauricio Carvalho <mauricio.cac@hotmail.com>
2022-09-29 14:20:50 -0300

    Criando step baseado em chunk

--- src/main/java/com/mrc/batch/config/step/StepConfig.java
+++ src/main/java/com/mrc/batch/config/step/StepConfig.java
@@ -1,11 +1,13 @@
 package com.mrc.batch.config.step;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,13 +19,20 @@ public class StepConfig {
 
 	@Bean
 	public Step step() {
-		return factory.get("step Configurado").tasklet(new Tasklet() {
+		return factory.get("step Configurado").<Integer, String>chunk(2).reader(itemReader()).processor(itemProcessor())
+				.writer(itemWriter()).build();
+	}
 
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println("STEP EXECUTADO");
-				return RepeatStatus.FINISHED;
-			}
-		}).build();
+	private IteratorItemReader<? extends Integer> itemReader() {
+		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+		return new IteratorItemReader<>(numeros.iterator());
+	}
+
+	private FunctionItemProcessor<? super Integer, ? extends String> itemProcessor() {
+		return new FunctionItemProcessor<Integer, String>(item -> item % 2 == 0 ? item + " é Par" : item + " é Impar");
+	}
+
+	private ItemWriter<? super String> itemWriter() {
+		return item -> item.forEach(System.out::println);
 	}
 }
